f3b58aa8e99f15b24100b0a60c0e53b5b25f0691
KaterynaKravchenko <kateryna.kravs@gmail.com>
2024-04-04 22:57:46 +0200

    Selector

--- src/test/java/com/ait/qa34/FindElementTest.java
+++ src/test/java/com/ait/qa34/FindElementTest.java
@@ -43,6 +43,77 @@ public class FindElementTest {
         System.out.println(elements.size());
 
     }
+    @Test
+    public void findElementByLocators(){
+        //id
+        driver.findElement(By.id("city"));
+        //class name
+        driver.findElement(By.className("input-container"));
+
+        //name By.name
+        driver.findElement(By.linkText("Let the car work"));
+
+        //partian link text
+        driver.findElement(By.partialLinkText("work"));
+    }
+    @Test
+    public void findElementByCssSelector(){
+        //tag name = tag name
+        // driver.findElement(By.tagName("h1"));
+        driver.findElement(By.cssSelector("h1"));
+        //id -> #id
+        // driver.findElement(By.id("city"));
+        driver.findElement(By.cssSelector("#city"));
+        //class name -> .className
+        // driver.findElement(By.className("input-container"));
+        driver.findElement(By.cssSelector(".input-container"));
+        //[name='user']
+        driver.findElement(By.cssSelector("[for='city']"));
+        driver.findElement(By.cssSelector("[href='/search']"));
+        //contains -> *
+        driver.findElement(By.cssSelector("[href*='car']"));
+        //start -> ^
+        driver.findElement(By.cssSelector("[href^='/let']"));
+        //end on -> $
+        driver.findElement(By.cssSelector("[href$='work']"));
+
+
+    }
+
+    //xPath//*[@attr='value']
+    @Test
+    public void findElementByXpath() {
+        // tag name = //tag
+        // driver.findElement(By.tagName("h1"));
+        driver.findElement(By.xpath("//h1"));
+        // id -> //tag[@id='idName']
+        // driver.findElement(By.id("city"));
+        driver.findElement(By.xpath("//*[@id='city']"));
+        //class name -> //tag[@class='className']
+        // driver.findElement(By.className("input-container"));
+        driver.findElement(By.xpath("//*[@class='input-container']"));
+        //contains -> //*[contains(@attr,'....')]
+        // driver.findElement(By.cssSelector("[href*='car']"));
+        driver.findElement(By.xpath("//*[contains(@href, 'car')]"));
+        //start->//*[starts-with(@attr, '....')
+
+
+        //text
+        driver.findElement(By.xpath("//*[starts-with(@href, '/let')]"));
+        driver.findElement(By.xpath("//span[text()=' Never mistaken for anything else ']"));
+        driver.findElement(By.xpath("//span[.=' Never mistaken for anything else ']"));
+        driver.findElement(By.xpath("//span[contains(.,'mistake')]"));
+
+        //search of parent element - //input[@id='username']/..
+        // //div/a - child - css=div>a
+        // //div//a -any lebel child - css = div a
+
+        //tag1[.tag2[@attr='value']]
+        //*[@id='firstName' or @name='lastName']
+        //*[@id='firstName' and @name='lastName']
+
+    }
+
 
 
 
